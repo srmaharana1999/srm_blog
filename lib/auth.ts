705900692b9eda42a63933b24abf691cb8faf059
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-10-11 02:32:26 +0530

    feat: add next/auth function for authentication

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,71 +1,74 @@
 import { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 import dbConnect from "./dbConnect";
 
 import bcrypt from "bcryptjs";
 import User from "./models/User";
 export const authOptions: NextAuthOptions = {
-    providers:[
-        CredentialsProvider({
-            name:'Credentials',
-            credentials:{
-                email:{label:"Email",type:"text",placeholder:"abc@domain.com"},
-                password:{label:"Password",type:"password"}
-            },
-            async authorize(credentials){
-                if(!credentials?.email || !credentials?.password){
-                    throw new Error('Missing email or password.');
-                }
-                try{
-                    await dbConnect();
-                    const user = await User.findOne({email:credentials.email});
-                    if(!user){
-                        throw new Error('No user found!');
-                    }
-
-                    const isValid = await bcrypt.compare(credentials.password,user.password);
-
-                    if(!isValid){
-                        throw new Error('Invalid Password.');
-                    }
-                    return {
-                        id:user._id.toString(),
-                        email:user.email,
-                    };
-                }catch(error){
-                    console.log("Error at Provider.");
-                    throw error;
-                }
-            }
-        })
-    ],
-    callbacks:{
-        async jwt({token,user}) {
-            // console.log("user : ",user);
-            if(user){
-                token.id = user.id;
-                token.email = user.email;
-            }
-            // console.log("token : ",token);
-            return token;     
-        },
-        async session({session,token}){
-            // console.log("session : ",session);
-            if(session.user && token){
-                session.user.id = token.id as string
-                session.user.email = token.email as string
-            }
-            // console.log("session.user : ",session.user);
-            return session;
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "abc@domain.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password.");
         }
+        try {
+          await dbConnect();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("No user found!");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!isValid) {
+            throw new Error("Invalid Password.");
+          }
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          console.log("Error at Provider.");
+          throw error;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      console.log("user : ", user);
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+      }
+      console.log("token : ", token);
+      return token;
     },
-    pages:{
-        signIn:"/sign-in",
-        error:"/sign-in"
+    async session({ session, token }) {
+      console.log("session : ", session);
+      if (session.user && token) {
+        session.user.id = token.id as string;
+        session.user.email = token.email as string;
+      }
+      console.log("session.user : ", session.user);
+      return session;
     },
-    session:{
-        strategy:"jwt",
-        maxAge:30*24*60*60
-    },
-    secret:process.env.NEXTAUTH_SECRET,
-}
+  },
+  pages: {
+    signIn: "/sign-in",
+    error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
