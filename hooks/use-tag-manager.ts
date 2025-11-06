dc29dd33ac39eb46bc7fad716b53001b0c21e7fc
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-11-06 06:50:58 +0530

    fix:remove unneccesary hooks

--- hooks/use-tag-manager.ts
+++ hooks/use-tag-manager.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 // Custom Hook for Tag Management
 const useTagManager = (maxTags: number) => {
