2fbf9ff0cc6f623f3eedbedd243a6be6a0ccc7b0
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-11-06 06:54:16 +0530

    fix: fix indention

--- app/api/imagekit-auth/route.ts
+++ app/api/imagekit-auth/route.ts
@@ -4,16 +4,23 @@ import { NextResponse } from "next/server";
 const imagekit = {
   publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLICKEY! as string,
   privateKey: process.env.IMAGEKIT_PRIVATEKEY! as string,
-//   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URLENDPOINT!,
+  //   urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URLENDPOINT!,
 };
 
 export async function GET() {
-    try{
-        const {token,expire,signature} = getUploadAuthParams(imagekit);
-        return NextResponse.json({token,expire,signature,publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLICKEY});
-    }catch(error){
-        console.log(error);
-        return NextResponse.json({error:"Imagekit Auth Failed!"},{status:500})
-    }
- 
+  try {
+    const { token, expire, signature } = getUploadAuthParams(imagekit);
+    return NextResponse.json({
+      token,
+      expire,
+      signature,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLICKEY,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "Imagekit Auth Failed!" },
+      { status: 500 }
+    );
+  }
 }
--- middleware.ts
+++ middleware.ts
@@ -1,45 +1,48 @@
-import { withAuth } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware";
 
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
     try {
-      console.log(req.nextauth.token);
+      console.log("middleware", req.nextauth.token);
     } catch (error) {
       console.error("Error in middleware:", error);
     }
   },
   {
     callbacks: {
-      authorized: ({ token,req }) => {
-        const {pathname} = req.nextUrl;
+      authorized: ({ token, req }) => {
+        const { pathname } = req.nextUrl;
         // console.log(req.nextUrl);
 
-        if(pathname.startsWith("/api/auth") 
-            || pathname.startsWith("/api/user") 
-            || pathname.startsWith("/api/post") 
-            || pathname == "/sign-up/verify-email" 
-            || pathname == "/sign-up" 
-            || pathname == "/api/imagekit-auth"
-            || pathname == "/sign-in"
-          ){
-                return true;
-            }
+        if (
+          // pathname.startsWith("/api/auth") ||
+          // pathname.startsWith("/api/user") ||
+          // pathname.startsWith("/api/post") ||
+          // pathname.startsWith("/api/categories") ||
+          pathname.startsWith("/api") ||
+          pathname.startsWith("/blogs") ||
+          pathname.startsWith("/test") ||
+          pathname == "/sign-up/verify-email" ||
+          pathname == "/sign-up" ||
+          pathname == "/api/imagekit-auth" ||
+          pathname == "/sign-in"
+        ) {
+          return true;
+        }
 
-          if(pathname ==="/" || pathname==="/blogs"){
-            return true;
-          }
+        if (pathname === "/" || pathname === "/blogs") {
+          return true;
+        }
 
         return !!token;
       },
     },
-  },
-)
+  }
+);
 
 export const config = {
-    matcher:[
-        
-       "/((?!_next/static|_next/image|favicon.ico|images|fonts|media|uploads|logo|.*\\.png$|.*\\.jpg$|.*\\.webp$).*)",
-    ]
-}
-    
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|images|fonts|media|uploads|logo|.*\\.png$|.*\\.jpg$|.*\\.webp$).*)",
+  ],
+};
