eaf2e1d756b03cd9ba0504642001e609a70e23cf
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-11-06 06:55:53 +0530

    style: enhance styling

--- app/(content)/page.tsx
+++ app/(content)/page.tsx
@@ -6,17 +6,24 @@ import Image from "next/image";
 const Home = () => {
   return (
     <div className="mt-25 max-w-7xl mx-auto w-full">
-      <div className="w-11/12 mx-auto flex flex-col-reverse md:flex-row text-white gap-6 md:gap-0">
-        <div className="w-full md:w-1/2 bg-amber-90 flex flex-col gap-6 md:justify-between md:gap-0 text-center md:text-left">
-          <h1 className="text-5xl lg:text-7xl font-semibold ">
-            <p className="text-black inline">blog IT.</p>
-            <p className="text-fuchsia-500 inline">post IT.</p> <br />
-            <span className="text-blue-500">Rule the Feed!</span>
+      <div className="w-11/12 h-[calc(100vh-10rem)] mx-auto flex flex-col-reverse items-center justify-around md:flex-row text-white gap-6 md:gap-0">
+        <div className="w-full md:w-1/2 bg-amber-90 md:gap-0 text-center md:text-left">
+          <h1 className="text-5xl lg:text-7xl font-semibold">
+            <p className="text-black inline">
+              <span className="font-bold">blog</span> IT.
+            </p>
+            <p className="text-fuchsia-500 inline">
+              <span className="font-bold">post</span> IT.
+            </p>{" "}
+            <br />
+            <span className="text-blue-500">
+              Rule the <span className="font-bold">Feed</span>!
+            </span>
           </h1>
-          <h3 className="text-2xl lg:text-3xl text-black ">
+          <h3 className="text-2xl lg:text-3xl text-black my-4">
             Got something to say?{" "}
           </h3>
-          <p className="md:text-xs lg:text-lg tracking-widest">
+          <p className="md:text-xs lg:text-lg text-chart-5 tracking-widest mb-14">
             Start your own{" "}
             <span className="text-fuchsia-500 font-semibold">post</span>, share
             cool ideas, post fun stuff, and see what others are writing too.
@@ -24,16 +31,15 @@ const Home = () => {
           </p>
           <CTA />
         </div>
-        <div className="h-full w-full md:w-1/2 justify-items-center md:justify-items-end-safe">
-          <div className="w-fit h-fit bg-gradient-to-r from-violet-200 to-pink-200 rounded-2xl border-2 border-border">
-            <Image
-              src="/images/hero-image.png"
-              alt="hero_image"
-              height={500}
-              width={500}
-              className="object-containb "
-            />
-          </div>
+
+        <div className="w-fit h-fit bg-gradient-to-r from-violet-200 to-pink-200 rounded-2xl border-2 border-border">
+          <Image
+            src="/images/hero-image.png"
+            alt="hero_image"
+            height={500}
+            width={500}
+            className="object-containb "
+          />
         </div>
       </div>
       <div className="w-11/12 mx-auto my-16">
