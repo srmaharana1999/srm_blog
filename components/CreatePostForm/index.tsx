adb21b0a54ca2132a4cdbb271406f9edad9e5891
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-11-23 02:27:42 +0530

    feat: connect create blog form with backend and DB

--- components/CreatePostForm/index.tsx
+++ components/CreatePostForm/index.tsx
@@ -1,67 +1,109 @@
 "use client";
 
 import TextField from "../Inputs/TextField";
-import { Form, Formik } from "formik";
+import { Form, Formik, FormikHelpers } from "formik";
 import ImageUpload from "../ImageUpload";
 import { SimpleEditor } from "../TipTapRTE/tiptap-templates/simple/simple-editor";
-import { initialBlogPostValues, postValidationSchema } from "@/lib/Schema/blog";
+import {
+  BlogPostType,
+  initialBlogPostValues,
+  postValidationSchema,
+} from "@/lib/Schema/blog";
 import { Button } from "../ui/button";
 import AdvancedTagInput from "./TagInput";
 import CategoryInput from "./CategoryInput";
 import { useState } from "react";
 import { UploadResponse } from "@imagekit/next";
 import StatusSelector from "./StatusSelector";
+import { Loader2Icon } from "lucide-react";
+import { apiClient } from "@/lib/apiClient";
+import { useCategoryState } from "@/store/useCategoryStore";
+import { useTagState } from "@/store/useTagStore";
 
 const CreateBlogPostForm = () => {
   const [blogImage, setBlogImage] = useState<UploadResponse>();
-  console.log(blogImage);
+  const { categories } = useCategoryState();
+  const { tags } = useTagState();
+  const handleBlogSubmit = async (
+    values: BlogPostType,
+    formikHelpers: FormikHelpers<BlogPostType>
+  ) => {
+    const categoryObj = await categories.find((cat) =>
+      cat.catName.toLowerCase().includes(values.categoryId.toLowerCase())
+    );
+    const tagIds = await tags
+      .map((tagObj) =>
+        values.tagIds.includes(tagObj.tagName) ? String(tagObj._id) : undefined
+      )
+      .filter((id): id is string => typeof id === "string");
+    try {
+      const response = await apiClient.createBlog({
+        title: values.title,
+        featuredImage: blogImage?.url || "",
+        content: values.content,
+        status: values.status,
+        tagIds: Array.from(tagIds),
+        categoryId: String(categoryObj?._id),
+        allowComments: true,
+      });
+      console.log("handleBlogSubmit", response);
+    } catch (error) {
+      console.log("handleBlogSubmit", error);
+    } finally {
+      formikHelpers.setSubmitting(false);
+    }
+  };
   return (
     <Formik
       initialValues={initialBlogPostValues}
       validationSchema={postValidationSchema}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={handleBlogSubmit}
     >
-      <Form className="w-full space-y-8">
-        <div>
-          <h2 className="text-xl">Title:</h2>
-          <p className="text-xs text-chart-5 mb-2">
-            Enter a catchy and descriptive title for your post.
-          </p>
-          <TextField
-            name="title"
-            type="text"
-            placeholder="Enter descriptive title for your blog post"
-          />
-        </div>
-        <div>
-          <h2 className="text-xl">Featured Image:</h2>
-          <p className="text-xs text-chart-5 mb-2">
-            Upload a main image that represents your post.
-          </p>
-          <div className="h-96">
-            <ImageUpload onSuccess={setBlogImage} />
+      {({ isSubmitting }) => (
+        <Form className="w-full space-y-8">
+          <div>
+            <h2 className="text-xl">Title:</h2>
+            <p className="text-xs text-chart-5 mb-2">
+              Enter a catchy and descriptive title for your post.
+            </p>
+            <TextField
+              name="title"
+              type="text"
+              placeholder="Enter descriptive title for your blog post"
+            />
           </div>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-5 gap-6 md:gap-16">
-          <div className="md:col-span-3 flex flex-col justify-between">
-            <CategoryInput name="categoryId" />
-            <AdvancedTagInput name="tags" />
+          <div>
+            <h2 className="text-xl">Featured Image:</h2>
+            <p className="text-xs text-chart-5 mb-2">
+              Upload a main image that represents your post.
+            </p>
+            <div className="h-96">
+              <ImageUpload onSuccess={setBlogImage} />
+            </div>
           </div>
-          <div className="md:col-span-2">
-            <StatusSelector name="status" />
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-6 md:gap-16">
+            <div className="md:col-span-3 flex flex-col justify-between">
+              <CategoryInput name="categoryId" />
+              <AdvancedTagInput name="tagIds" />
+            </div>
+            <div className="md:col-span-2">
+              <StatusSelector name="status" />
+            </div>
           </div>
-        </div>
-        <div>
-          <h3 className="text-xl">Content:</h3>
-          <p className="text-xs text-chart-5 mb-2">
-            Write the full content of your post here.
-          </p>
+          <div>
+            <h3 className="text-xl">Content:</h3>
+            <p className="text-xs text-chart-5 mb-2">
+              Write the full content of your post here.
+            </p>
 
-          <SimpleEditor name="content" />
-        </div>
+            <SimpleEditor name="content" />
+          </div>
 
-        <Button className=" text-lg w-full">Submit</Button>
-      </Form>
+          <Button className=" text-lg w-full">
+            {isSubmitting ? <Loader2Icon className="animate-spin" /> : "Submit"}
+          </Button>
+        </Form>
+      )}
     </Formik>
   );
 };
