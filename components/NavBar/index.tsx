239b5af5332666d4d7a2ff2bf13e2a9573176778
Soumya Ranjan Maharana <s.r.m.20.3.2018@gmail.com>
2025-12-03 03:25:45 +0530

    style: navbar set to full width

--- components/NavBar/index.tsx
+++ components/NavBar/index.tsx
@@ -22,7 +22,7 @@ const NavBar = () => {
   }, [fetchCategories, fetchTags]);
   return (
     <div className="group fixed w-full left-1/2 -translate-x-1/2 top-0 bg-[#cccccc]  border-b-1 border-border z-100 ">
-      <div className=" max-w-7xl mx-auto w-full h-20 text-black flex justify-between items-center px-4">
+      <div className=" mx-auto h-20 text-black flex justify-between items-center px-4">
         <div className="text-black bg-white cursor-pointer lower-bar p-2 border-shadow">
           <div className="flex text-lg sm:text-3xl text-chart-1 left-bar">
             <Image
@@ -48,7 +48,7 @@ const NavBar = () => {
         <div>
           <ul className="list-none hidden lg:flex gap-10 text-md ">
             <NavLink label="Home" href="/" />
-            <NavLink label="Blogs" href="/test" />
+            <NavLink label="Blogs" href="/blogs" />
             <NavLink label="Contact Us" href="#contact-us" />
           </ul>
         </div>
@@ -69,7 +69,7 @@ const NavBar = () => {
               </Link>
             </div>
           ) : (
-            <div className="hidden lg:flex gap-4">
+            <div className="hidden lg:flex gap-4 lg:mr-16">
               {session.user ? (
                 <UserMenu
                   id={String(session.user.id)}
